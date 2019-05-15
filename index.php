50bbb0d132ab968c79499ccb4c0a6e0b10980e1c
Alex-MartinTanui <50584983+Alex-MartinTanui@users.noreply.github.com>
2019-05-15 15:21:07 +0300

    first website upload

---
+++ index.php
@@ -0,0 +1,117 @@
+
+<alex martin photogtaphy>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+$title = "Home";
+$content = " Alex photography";
+
+include 'template.php'
+
+?>;
+<img src="images/nature 1.jpg" class="imgLeft"
+<h3> THE JOURNEY</h3>
+<p>
+    
+I set out for a journey 
+It's end I know not
+A journey to break the norms
+A journey to break the traditions
+Traditions of my forefathers 
+A journey to set me free
+Free from this bondage
+
+I refuse 
+Refuse to be ruled by my past
+A past that has filled my path with dust
+A past that has harboured pain in my heart
+A past that has left a poor wretch hurt
+
+I'm out on this journey 
+A journey with a purpose 
+To bring life to a living carcass 
+To unfold the hidden potential 
+To break the covenants of my traditions
+Traditions with meaningless conditions 
+My missions in this journey must be achieved 
+Before they are archived
+
+In this journey 
+Despite the challenges 
+Despite the indifferences 
+Nothing will hinder me
+Nothing will kill my spirit 
+My spirit to fight
+Fight for my freedom 
+Until justice prevails
+
+My success 
+My job
+The love of my life
+My unborn kids
+My future to be precise 
+Is bound in my traditions
+Traditions my foot!!! 
+
+Not until I find justice 
+Not until I'm set free
+Not until iam unchained 
+From my clueless norms and traditions 
+I will keep going down the journey 
+My journey for freedom 
+
+I will fight like a warrior 
+I will stand strong and firm like a rock
+To unchain myself and generation at large
+From the ancestral bondage 
+If you can't help me fight
+Then watch me in this journey 
+ 
+</p>
+<img src="images/fire.JPG" class="imgLeft" />
+  
+<h3> BROKEN ANGEL </h3>
+<p>
+    
+I don't know why but when I saw you first
+I didn't see your angelic strength
+No I felt the despair dancing on your broken wings
+The silent cracks playing in your beautiful armor
+
+Like a bird striving to fly
+You fought the dark eating you from within
+You forced a smile to your face even when your soul said no
+And that was your true victory
+
+Like a broken winged bird 
+You fought the storms
+Tearing yourself piece by piece
+Until you were no more
+But no matter what they say that was your true strength 
+
+Like a caged bird you fought in the silence of the night
+Crying in pain and yet never despairing 
+Fighting the dark clouds tearing you apart 
+And that's your true victory 
+
+A broken angel racing the fates forced upon 
+And smiling even when the dark threatened to tear down
+And like a free healing bird
+Swarming the skies with cries of silent war
+
+*a true rose that kept the thorns only for safety you were*
+
+
+
+</p>
+<img src="images/gloria.jpg" class="imgRight" />
+<h3> Love Gloria </h3>
+<p>
+   To the one ill never let go, the love of my life that turned my life crazy,
+   up side down, left and right. I never knew this fetal but still i rose again,
+   give me one more chance to clear my mistakes; trust me you will never regrate.
+   #love Alex
+</p>
---
+++ template.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $title; ?></title>
+        <link rel="stylesheet" type="text/css" href="styles/stylesheet.css"/>
+    </head>
+    <body>
+      <div id="wrapper">
+          <div id="banner">
+                      </div>
+          <nav id="navigation>"> 
+              <ul id="nav"> 
+                  <li><a href="index.php">home</a></li> 
+                   <li><a href="#">photography</a></li>
+                    <li><a href="#">creativity</a></li>
+                     <li><a href="#">about</a></li>
+              </ul>
+          </nav>
+              
+          </nav> 
+          <div id="content_area"> 
+              <?php echo $content; ?>
+          </div> 
+          
+          <div id="sidebar"> 
+              
+          </div> 
+          <p> what the hearts feels the mind thinks.
+             a link between pictures and poetry </p>
+          <p>
+              between the heart and their lies a great distance of dilemma find 
+              out how. 
+             
+          </p>
+          </p>
+          
+          <footer>
+              <p> All rights reserved</p> 
+          </footer> 
+          
+      </div>
+    </body>
+</html>
